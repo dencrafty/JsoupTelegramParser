583b95e9d5e9a4f67c4d7c501377e4a01067107d
den <dencrafty@gmail.com>
2024-02-28 03:48:22 +0300

    Add action done button listener.

--- app/src/main/java/dev/dencrafty/tgparser/ui/StartFragment.kt
+++ app/src/main/java/dev/dencrafty/tgparser/ui/StartFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import androidx.navigation.Navigation
 import dev.dencrafty.tgparser.databinding.FragmentStartBinding
 
@@ -19,10 +21,23 @@ class StartFragment : Fragment() {
         val channelField = binding.channel
 
         startBtn.setOnClickListener {
-            val action = StartFragmentDirections.actionStartFragmentToPagingFragment(channelField.text.toString().trim())
-            Navigation.findNavController(startBtn).navigate(action)
+            startPagingScreen(channelField)
+        }
+
+        channelField.setOnEditorActionListener { textView, action, event ->
+            if (action == EditorInfo.IME_ACTION_DONE) {
+                startPagingScreen(textView as EditText)
+            }
+            false
         }
 
         return binding.root
+    }
+
+    private fun startPagingScreen(channelField: EditText) {
+        val action = StartFragmentDirections.actionStartFragmentToPagingFragment(
+            channelField.text.toString().trim()
+        )
+        Navigation.findNavController(channelField).navigate(action)
     }
 }
